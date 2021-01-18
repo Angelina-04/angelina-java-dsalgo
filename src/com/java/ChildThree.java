9b3cda25c680d0e46007b708ec1ffa2e56c978a0
Angelina-04 <61914750+Angelina-04@users.noreply.github.com>
2021-01-19 00:02:09 +0530

    Inheritance

---
+++ src/com/java/ChildThree.java
@@ -0,0 +1,12 @@
+package com.Inheritance;
+
+public class ChildThree extends Parents {
+	
+	public String getName() {
+		return "Timothy";
+	}
+		public String getBloodGroup() {
+			return "AB Positive";
+		}
+}
+
---
+++ src/com/java/ThirdPartyObjs.java
@@ -0,0 +1,18 @@
+package com.Inheritance;
+
+public class ThirdPartyObjs {
+             public static ChildOne getChildOneDetails() {
+            	 ChildOne childOneobj=new ChildOne();
+            	 return childOneobj;
+             }
+             public static ChildTwo getChildTwoDetails() {
+            	 ChildTwo childTwoobj=new ChildTwo();
+            	 return childTwoobj;
+             }
+             public static ChildThree getChildThreeDetails() {
+            	 ChildThree  childThreeobj=new ChildThree();
+            	 return childThreeobj;
+             }
+             
+	
+}
